f1caf9325f5fa29274c2dc0bc3fe041759472fd4
1110236 <915840469@qq.com>
2022-07-01 15:30:32 +0800

    Pilot with Georgia, P3, data, prepared for P4 Zhou Chen.

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     # Ada_pygame_prototype.run_prototype()
 
     # Do the pilot studies. Do not run this with prototype.
-    Ada_pygame_prototype.run_pilots(name="Georgia",
+    Ada_pygame_prototype.run_pilots(name="Zhou Chen",
                                     time="1 July 2022",
-                                    id_participant=3)
+                                    id_participant=4)
